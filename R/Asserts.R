c6774667e53df91eb9ca036c7887927abd0258b4
mikebesso <mike.besso@gmail.com>
2017-10-26 20:56:52 -0500

    more asserts

--- R/Asserts.R
+++ R/Asserts.R
@@ -11,3 +11,25 @@ AssertFileExists <- assertive.files::assert_all_are_existing_files
 
 #' @export
 AssertFilesExists <- assertive.files::assert_all_are_existing_files
+
+#' @export
+AssertEquals <- assertive::assert_all_are_equal_to
+
+
+#' @export
+AssertAllEquals <- assertive::assert_all_are_equal_to
+
+#' @export
+AssertAnyEquals <- assertive::assert_any_are_equal_to
+
+#' @export
+AssertAllNotEquals <- assertive::assert_all_are_not_equal_to
+
+#' @export
+AssertNotEquals <- assertive::assert_any_are_not_equal_to
+
+#' @export
+AssertAllMatchFixed <- assertive::assert_all_are_matching_fixed
+
+#' @export
+AssertAllMatchPattern <- assertive::assert_all_are_matching_regex
