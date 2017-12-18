818125fee848158f8ff3c5e4fb402cf855c17dc6
mikebesso <mike.besso@gmail.com>
2017-12-17 19:53:13 -0600

    cleaning up for blog

--- R/attributes_plus.R
+++ R/attributes_plus.R
@@ -1,11 +1,21 @@
 
 #' @export
 TransferAttributes <- function(source, destination){
+
+  # TODO
+  #   Consider copying over attributes that exist
+  #   Consider copying/merging classes
+
+  # Get that attributes from the source
   SourceAttributes <- attributes(source)
+
+  # Copy the attributes from the destination that do not exist in the source
   for(attribute in names(SourceAttributes)){
     if(!(attribute %in% names(attributes(destination)))){
       attr(destination, attribute) <- SourceAttributes[[attribute]]
     }
   }
+
+  # Return the destination with the copied attributes
   return(destination)
 }
